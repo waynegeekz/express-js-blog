c2a3fa2faea0e283f75edf5c03a83c3bdcbd158a
Chris Wayne Comendador <cwayne.comendador@gmail.com>
2020-08-08 15:37:32 +0800

    display mock articles in homepage

--- server.js
+++ server.js
@@ -7,7 +7,19 @@ app.set('view engine', 'ejs');
 app.use('/articles', articleRouter);
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const articles = [
+        {
+            title : "Test Article",
+            created: new Date(),
+            description: "This excerpt will explain the details of the website."
+        },
+        {
+            title : "Second Article",
+            created: new Date(),
+            description: "This excerpt will explain the details of the website."
+        }
+    ];
+    res.render('index', { articles: articles});
 });
 
 
