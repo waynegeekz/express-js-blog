f18202fbcebd7abaf04f2308bba12bdde1ca0747
Chris Wayne Comendador <cwayne.comendador@gmail.com>
2020-08-08 17:35:34 +0800

    persist data when submitting article form

--- routes/articles.js
+++ routes/articles.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Article = require('./../models/articles');
 
 
 router.get('/', (req, res) => {
@@ -19,10 +20,26 @@ router.get('/', (req, res) => {
 });
 
 router.get('/add', (req, res) => {
-    res.render('articles/add');
+    res.render('articles/add', { 'article': new Article() });
 });
 
-router.post('/', (req, res) => {
+router.get('/view/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
+router.post('/', async (req, res) => {
+    let article = new Article({
+        title : req.body.title,
+        description: req.body.description,
+        markdown: req.body.markdown
+    });
+
+    try {
+        article = await article.save();
+        res.redirect(`/articles/view/${article.id}`)
+    } catch (e) {
+        res.render('articles/add', { 'article': article });
+    }
 
 });
 
--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const articleRouter = require ('./routes/articles')
 const app = express();
 
+mongoose.connect('mongodb://localhost/blog',{ 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
 app.set('view engine', 'ejs');
 
-app.use('/articles', articleRouter);
+
+app.use(express.urlencoded( { extended: false }));
 
 /* ROUTE TO HOMEPAGE */
 app.get('/', (req, res) => { 
     res.render('index');
 });
 
+app.use('/articles', articleRouter);
 
 app.listen(5000);
