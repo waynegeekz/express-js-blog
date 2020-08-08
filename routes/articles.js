c81e9b0bd83388e6bd219bd41ed4181496c286d1
Chris Wayne Comendador <cwayne.comendador@gmail.com>
2020-08-08 18:11:15 +0800

    display all articles in article home page

--- routes/articles.js
+++ routes/articles.js
@@ -3,19 +3,10 @@ const router = express.Router();
 const Article = require('./../models/articles');
 
 
-router.get('/', (req, res) => {
-    const articles = [
-        {
-            title : "Test Article",
-            created: new Date(),
-            description: "This excerpt will explain the details of the website."
-        },
-        {
-            title : "Second Article",
-            created: new Date(),
-            description: "This excerpt will explain the details of the website."
-        }
-    ];
+router.get('/', async(req, res) => {
+    const articles = await Article.find().sort({
+        created: 'desc'
+    });
     res.render("articles/index", { 'articles' : articles });
 });
 
@@ -23,8 +14,15 @@ router.get('/add', (req, res) => {
     res.render('articles/add', { 'article': new Article() });
 });
 
-router.get('/view/:id', (req, res) => {
-    res.send(req.params.id);
+router.get('/view/:id', async(req, res) => {
+    const article = await Article.findById(req.params.id);
+
+    if(article === null) {
+        res.redirect('/article');
+    }
+
+    res.render('articles/view', {'article': article })
+
 });
 
 router.post('/', async (req, res) => {
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const articleRouter = require ('./routes/articles')
+const articleRouter = require ('./routes/articles');
 const app = express();
 
 mongoose.connect('mongodb://localhost/blog',{ 
@@ -14,7 +14,10 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded( { extended: false }));
 
 /* ROUTE TO HOMEPAGE */
-app.get('/', (req, res) => { 
+app.get('/', async (req, res) => { 
+    const articles = await Article.find().sort({
+        created: 'desc'
+    });
     res.render('index');
 });
 
