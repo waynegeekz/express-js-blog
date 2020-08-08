f8da284d732024d022d2c96d8170e953bcb62530
Chris Wayne Comendador <cwayne.comendador@gmail.com>
2020-08-08 16:07:37 +0800

    create add article page and transfer home articles to articles page

--- routes/articles.js
+++ routes/articles.js
@@ -3,7 +3,27 @@ const router = express.Router();
 
 
 router.get('/', (req, res) => {
-    res.send("In Article Page");
+    const articles = [
+        {
+            title : "Test Article",
+            created: new Date(),
+            description: "This excerpt will explain the details of the website."
+        },
+        {
+            title : "Second Article",
+            created: new Date(),
+            description: "This excerpt will explain the details of the website."
+        }
+    ];
+    res.render("articles/index", { 'articles' : articles });
+});
+
+router.get('/add', (req, res) => {
+    res.render('articles/add');
+});
+
+router.post('/', (req, res) => {
+
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,20 +6,9 @@ app.set('view engine', 'ejs');
 
 app.use('/articles', articleRouter);
 
-app.get('/', (req, res) => {
-    const articles = [
-        {
-            title : "Test Article",
-            created: new Date(),
-            description: "This excerpt will explain the details of the website."
-        },
-        {
-            title : "Second Article",
-            created: new Date(),
-            description: "This excerpt will explain the details of the website."
-        }
-    ];
-    res.render('index', { articles: articles});
+/* ROUTE TO HOMEPAGE */
+app.get('/', (req, res) => { 
+    res.render('index');
 });
 
 
